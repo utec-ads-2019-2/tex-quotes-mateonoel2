dc9a7bcf506bd05078739739b2d41dd2c331b30e
mateonoel2 <39866695+mateonoel2@users.noreply.github.com>
2019-08-22 20:48:31 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,44 @@
 #include <iostream>
-
+#include <string>
+#include <queue>
 using namespace std;
 
-int main(int argc, char *argv[]) {
+int main() {
+    string text;
+
+    int counter = 0;
+
+    auto *Queue = new queue<string>;
+
+    while(getline(cin, text)){
+
+        if(text.empty()){
+            break;
+        }
+
+        for(int i=0; i<text.size(); i++) {
+            if (text[i] == '\"') {
+                if(counter==0){
+                    text.replace(text.find('\"'), 1, "``");
+                }
+                else if(counter==1){
+                    text.replace(text.find('\"'), 1, "''");
+                }
+                counter++;
+
+                if(counter==2){
+                    counter=0;
+                }
+            }
+        }
+
+        Queue->push(text);
+    }
+
+    while(!Queue->empty()){
+        cout << Queue->front()<<'\n';
+        Queue->pop();
+    }
+
     return 0;
 }
